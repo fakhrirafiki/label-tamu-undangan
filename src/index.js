6d7b998c2360e99b7f8ffef2e0d36a0d967339c6
Fakhri Rafiki <fakhri.rafiki@gmail.com>
2023-07-19 05:31:43 +0700

    hmm

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7lImMav5XCzpjc-CPW2VQhnEp-upTbV4",
+  authDomain: "wedding-card-1158f.firebaseapp.com",
+  projectId: "wedding-card-1158f",
+  storageBucket: "wedding-card-1158f.appspot.com",
+  messagingSenderId: "971427397253",
+  appId: "1:971427397253:web:096dbed906a1ca4c0333ec",
+  measurementId: "G-DSV2RVHRMW",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+//
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
